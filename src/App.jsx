33e45d138bd1f3cea7151a226f869f8c77991ac5
Andre Herter <andre-herter@web.de>
2025-08-14 09:42:23 +0200

    Fix: set Router basename for correct routing under subpath

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,20 @@
 import "./App.css";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Root from "./routes/root/Root";
 import Homepage from "./routes/homepage/Homepage";
 import Impressum from "./components/impressum/Impressum";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Root />,
-    children: [
-      {
-        index: true,
-        element: <Homepage />,
-      },
-      { path: "impressum", element: <Impressum /> },
-    ],
-  },
-]);
-
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <BrowserRouter basename="/react-tailwind-portfolio">
+      <Routes>
+        <Route path="/" element={<Root />}>
+          <Route index element={<Homepage />} />
+          <Route path="impressum" element={<Impressum />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
