f5bc4439ae24438427935ea22af019c96093d8aa
Andre Herter <andre-herter@web.de>
2025-08-14 16:12:46 +0200

    Fix: Update Impressum back link to work on GitHub Pages

--- src/components/impressum/Impressum.jsx
+++ src/components/impressum/Impressum.jsx
@@ -1,29 +1,25 @@
-import Button from "../button/Button";
+import { Link } from "react-router-dom";
 
 function Impressum() {
   return (
     <>
-      <Button
-        href="/"
-        className="flex flex-row-reverse gap-2 m-4"
-        text={"Zurück"}
-        svg={
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth="1.5"
-            stroke="currentColor"
-            className="size-6"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18"
-            />
-          </svg>
-        }
-      />
+      <Link to="/" className="flex flex-row-reverse flex-end gap-2 m-4">
+        Zurück
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth="1.5"
+          stroke="currentColor"
+          className="size-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18"
+          />
+        </svg>
+      </Link>
       <div className="text-center">
         <h2 className="text-3xl lg:4xl font-bold mt-28">Impressum</h2>
         <h3 className="text-xl lg:text-2xl font-bold mt-8">
